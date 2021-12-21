7feb81608de923a990f08d863f6c78b57d056e8b
Brewhouse Digital <zach@brewhouse.digital>
2021-12-20 21:53:05 -0600

    updated background switcher

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,10 +6,17 @@
     onMount(() => {
         fairyDustCursor();
     })
+
+    function handleBackgroundSwitch() {
+        document.body.classList.toggle("different-bg")
+    }
 </script>
 
 
 <div class="container">
+    <button class="btn background-switcher"
+            type="button" on:click={handleBackgroundSwitch}>🌟</button>
+
     <Header />
 
     <main>
@@ -25,8 +32,19 @@
         background-attachment: fixed;
         font-family: cursive;
     }
+
+    :global(body.different-bg) {
+        background-image: url("/images/background.jpg");
+    }
+
     main {
         padding: 24px;
         /*background-color: rgba(255, 255, 255, 0.9);*/
+    }
+
+    .background-switcher {
+        position: absolute;
+        top: 8px;
+        right: 8px;
     }
 </style>
